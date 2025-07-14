fb436a44f0914c959aa8492651d22bde6e11f0e1
valgoncharov <99090572+valgoncharov@users.noreply.github.com>
2025-07-14 17:45:45 +0300

    Add model and fun

--- python_test/databases/spend_db.py
+++ python_test/databases/spend_db.py
@@ -1,7 +1,7 @@
 from typing import Sequence
-from sqlalchemy import create_engine, Engine, select
-from sqlalchemy.orm import Session
-from model.db.spend import Category
+from sqlalchemy import create_engine, Engine
+from sqlmodel import Session, select
+from python_test.model.db.spend import Category, Spend
 
 
 class SpendDb:
@@ -27,3 +27,17 @@ class SpendDb:
             statement = select(Spend).where(Spend.id.in_(spend_ids))
             session.exec(statement).all()
             session.commit()
+
+    def get_category_by_id(self, category_id: str) -> Category:
+        with Session(self.engine) as session:
+            statement = select(Category).where(Category.id == category_id)
+            return session.exec(statement).first()
+
+    def get_spend_by_id(self, spend_id: str) -> Spend:
+        with Session(self.engine) as session:
+            statement = select(Spend).where(Spend.id == spend_id)
+            return session.exec(statement).first()
+
+    def delete_categories_by_ids(self, categories_ids: list[str]):
+        for category_id in categories_ids:
+            self.delete_category(category_id)
