dbc811e7e22c9dbb0f618dc0cc1a6fc9256b8cdf
valgoncharov <99090572+valgoncharov@users.noreply.github.com>
2025-07-14 17:36:46 +0300

    Add model

--- python_test/model/db/spend.py
+++ python_test/model/db/spend.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from sqlmodel import SQLModel, Field
 from datetime import datetime
+from typing import Optional
 
 
 class UserName(BaseModel):
@@ -9,14 +10,26 @@ class UserName(BaseModel):
 
 class Category(SQLModel, table=True):
     id: str = Field(default=None, primary_key=True)
-    category: str = Field(index=True)
-    username: str = Field(index=True)
+    name: str
+    username: str
+    archived: bool
 
 
 class Spend(SQLModel, table=True):
     id: str = Field(default=None, primary_key=True)
     amount: float = Field(index=True)
     description: str = Field(index=True)
-    category: str = Field(index=True)
-    spendDate: datetime = Field(index=True)
+    category_id: str = Field(foreign_key="category.id")
+    spend_date: datetime = Field(index=True)
     username: str = Field(index=True)
+    currency: str
+
+
+class SpendAdd(BaseModel):
+    id: Optional[str] = None
+    spendDate: str
+    category: Category
+    currency: str = 'RUB'
+    amount: float
+    description: str = ''
+    username: str
